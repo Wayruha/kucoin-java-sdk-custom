49566f9cdb429338d2b1cdfcf57f6193e85aa146
RomanDiachuk <rwdiachuk@gmail.com>
2022-09-19 23:36:16 +0200

    fix Response.isSuccessfull

--- src/main/java/com/kucoin/sdk/rest/response/KucoinResponse.java
+++ src/main/java/com/kucoin/sdk/rest/response/KucoinResponse.java
@@ -12,6 +12,8 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import org.apache.commons.lang3.StringUtils;
 
+import static org.apache.commons.lang3.StringUtils.isEmpty;
+
 /**
  * Created by zicong.lu on 2018/12/14.
  */
@@ -20,6 +22,7 @@ import org.apache.commons.lang3.StringUtils;
 public class KucoinResponse<R> implements Serializable {
     private static final long serialVersionUID = 1L;
     private static final String SUCCESS_CODE = "200000";
+    private static final String SUCCESS_MSG = "success";
     private static final List<String> SUCCESS_CODES = List.of("200000", "200");
     protected String code;
     protected String msg;
@@ -27,7 +30,7 @@ public class KucoinResponse<R> implements Serializable {
     private R data;
 
     public boolean isSuccessful() {
-        return SUCCESS_CODES.contains(this.code) && StringUtils.isEmpty(msg);
+        return SUCCESS_CODES.contains(this.code) && (SUCCESS_MSG.equalsIgnoreCase(msg) || isEmpty(msg));
     }
 
 }
