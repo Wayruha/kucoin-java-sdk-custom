11e2c215726b12f2f19fc2a4781bbdadc403c0a4
RomanDiachuk <rwdiachuk@gmail.com>
2022-11-08 12:06:22 +0100

    AccountChangeEvent.RelationEventConstants

--- src/main/java/com/kucoin/sdk/websocket/event/AccountChangeEvent.java
+++ src/main/java/com/kucoin/sdk/websocket/event/AccountChangeEvent.java
@@ -3,16 +3,9 @@
  */
 package com.kucoin.sdk.websocket.event;
 
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import lombok.Data;
-import lombok.Getter;
 
-import java.io.IOException;
 import java.math.BigDecimal;
-import java.util.Arrays;
 import java.util.Map;
 
 /**
@@ -33,7 +26,7 @@ public class AccountChangeEvent {
 
     private BigDecimal holdChange;
 
-    private RelationEvent relationEvent;
+    private String relationEvent;
 
     private Map relationContext;
 
@@ -43,44 +36,27 @@ public class AccountChangeEvent {
 
     private String accountId;
 
-    @JsonDeserialize(using = RelationEventDeserializer.class)
-    public enum RelationEvent {
-        MAIN_DEPOSIT("main.deposit"),
-        MAIN_WITHDRAW_HOLD("main.withdraw_hold"),
-        MAIN_WITHDRAW_DONE("main.withdraw_done"),
-        MAIN_TRANSFER("main.transfer"),
-        MAIN_OTHER("main.other"),
-        TRADE_HOLD("trade.hold"),
-        TRADE_SETTED("trade.setted"),
-        TRADE_TRANSFER("trade.transfer"),
-        TRADE_OTHER("trade.other"),
-        MARGIN_HOLD("margin.hold"),
-        MARGIN_SETTED("margin.setted"),
-        MARGIN_TRANSFER("margin.transfer"),
-        MARGIN_ORDER("margin.other"),
-        OTHER("other");
+    public static class RelationEventConstants {
+        public static final String MAIN_DEPOSIT = "main.deposit";
+        public static final String MAIN_WITHDRAW_HOLD = "main.withdraw_hold";
+        public static final String MAIN_WITHDRAW_DONE = "main.withdraw_done";
+        public static final String MAIN_TRANSFER = "main.transfer";
+        public static final String MAIN_OTHER = "main.other";
+        public static final String TRADE_HOLD = "trade.hold";
+        public static final String TRADE_SETTED = "trade.setted";
+        public static final String TRADE_TRANSFER = "trade.transfer";
+        public static final String TRADE_OTHER = "trade.other";
+        public static final String MARGIN_HOLD = "margin.hold";
+        public static final String MARGIN_SETTED = "margin.setted";
+        public static final String MARGIN_TRANSFER = "margin.transfer";
+        public static final String MARGIN_ORDER = "margin.other";
 
-        @Getter
-        private String code;
+        private static final String ISOLATED_MARGIN_EVENT_REGEX = "isolated_\\w+-\\w+\\.\\w+";
 
-        RelationEvent(String code) {
-            this.code = code;
-        }
-
-        public static RelationEvent fromCode(String code) {
-            return Arrays.stream(values()).filter(v -> v.getCode().equals(code)).findAny().orElse(null);
+        public static String parseIsolatedSymbol(String event) {
+            if (event.matches(ISOLATED_MARGIN_EVENT_REGEX)) {
+                return event.split("_")[1].split("\\.")[0];
+            } else return null;
         }
     }
-
-    public static class RelationEventDeserializer extends StdDeserializer<RelationEvent> {
-        protected RelationEventDeserializer() {
-            super(RelationEvent.class);
-        }
-
-        @Override
-        public RelationEvent deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
-            return RelationEvent.fromCode(p.getText());
-        }
-    }
-
 }
